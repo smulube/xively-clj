77eeb3231f377756f9962251aa17a09e7f083a1a
Sam Mulube <sam.mulube@cosm.com>
2013-05-24 01:00:28 +0000

    Add basic spec for some core functions.

---
+++ spec/xively/core_spec.clj
@@ -0,0 +1,44 @@
+(ns xively.core-spec
+  (:require [speclj.core :refer :all]
+            [xively.core :as core]))
+
+(describe "keywordise"
+  (it "converts an underscored string returned to a clojure style keyword"
+    (should= :title (core/keywordise "title"))
+    (should= :product-id (core/keywordise "product_id")))
+  (it "throws an exception if passed a nil string"
+    (should-throw (core/keywordise nil))))
+
+(describe "stringify"
+  (it "converts a clojure style keyword into a string"
+    (should= "title" (core/stringify :title))
+    (should= "product_id" (core/stringify :product-id)))
+  (it "throws an exception if passed a nil keyword"
+    (should-throw (core/stringify nil))))
+
+(describe "stringify-keys"
+  (it "stringifies all the keys in a map"
+    (should= {"title" "title"} (core/stringify-keys {:title "title"}))))
+
+(describe "parse-json"
+  (it "parses a json string returning keyword keys"
+    (should= {:title "title", :product-id "12345"}
+             (core/parse-json "{\"title\":\"title\",\"product_id\":\"12345\"}"))
+    (should= nil (core/parse-json nil))))
+
+(describe "safe-parse"
+  (it "returns the parsed body for a 200 status code"
+    (should= {:title "title", :product-id "12345"}
+             (core/safe-parse {:headers "headers"
+                               :status 200
+                               :body "{\"title\":\"title\",\"product_id\":\"12345\"}"})))
+  (it "returns a not-modified keyword for a 304 status code"
+    (should= :xively.core/not-modified
+             (core/safe-parse {:status 304})))
+  (it "returns the original response but with a parsed body"
+    (should= {:headers "headers"
+              :status 500
+              :body {:error "Server error"}}
+             (core/safe-parse {:headers "headers"
+                               :status 500
+                               :body "{\"error\":\"Server error\"}"}))))
--- src/xively/core.clj
+++ src/xively/core.clj
@@ -4,17 +4,22 @@
 
 ;; Var containing the base URL for all API calls
 (def ^:dynamic *api-url* "https://api.xively.com/v2")
+
+;; Dynamic var in which we set our API key
 (def ^:dynamic *api-key* nil)
+
+;; User-Agent string
 (def ^:dynamic *user-agent*
   (str "xively-clj/0.1.0-SNAPSHOT (https://xively.com) Clojure/"
        (clojure-version)))
 
-; Return map of headers suitable for passing to API.
-(defn- headers
+(defn headers
+  "Return map of headers suitable for passing to the API, merging in user
+  supplied headers."
   [& user-headers]
   (merge {"X-ApiKey" *api-key*, "User-Agent" *user-agent*} user-headers))
 
-(defn- keywordise
+(defn keywordise
   "Convert key strings returned in JSON from the API into Clojure style
   keywords, i.e. replace underscores with hyphens."
   [s]
@@ -22,32 +27,33 @@
     (.replace "_" "-")
     keyword))
 
-(defn- stringify
+(defn stringify
   "Convert a Clojure style keyword (with hyphens), into an underscored string."
   [k]
   (-> k
     name
     (.replace "-" "_")))
 
-(defn- stringify-keys
+(defn stringify-keys
   "Create a map with keys converted to strings ready for sending to the Xively
   API."
   [m]
   (into (empty m)
-    (for [[k v] m] [(stringise k) v])))
+    (for [[k v] m] [(stringify k) v])))
 
-(defn- parse-json
-  "Same as json/parse-string but handles nil gracefully"
+(defn parse-json
+  "Same as json/parse-string but handles nil gracefully and returns keys as
+  keywords."
   [s]
   (when s (json/parse-string s keywordise)))
 
-(defn- safe-parse
+(defn safe-parse
   [{:keys [headers status body] :as response}]
   (cond
     (= 304 status)
       ::not-modified
     (#{204 400 401 403 404 422 500 503} status)
-      (update-in response [:body parse-json])
+    (update-in response [:body] parse-json)
     :else
       (parse-json body)))
 
@@ -60,6 +66,9 @@
 (defn api-call
   "Make an HTTP request to the API"
   [http-method path & [opts]]
-  (let [url (str *api-url* path)]
-    (safe-parse (http/request (merge {:method http-method, :url url, :headers
-                                      (headers)} opts)))))
+  (let [url (str *api-url* path)
+        headers (headers (:headers opts))
+        opts (dissoc opts :headers)]
+    (safe-parse (http/request (merge {:method http-method,
+                                      :url url,
+                                      :headers headers} opts)))))
