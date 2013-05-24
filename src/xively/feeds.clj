a43fc126c28b9c94e9adbe4e3a1b73b3ecb24ecb
Sam Mulube <sam.mulube@cosm.com>
2013-05-24 01:15:32 +0000

    Sketchy create feed implementation

--- src/xively/feeds.clj
+++ src/xively/feeds.clj
@@ -1,8 +1,22 @@
 (ns xively.feeds
   (:use [xively.core]))
 
+(defn- decorate-attributes
+  "Clean attributes map from nice clean Clojure form, into the format Xively expects"
+  ([feed]
+    (decorate-attributes :v2))
+  ([feed version]
+    feed))
+
 (defn feed
   "Return details of a feed"
   [feed-id]
   (let [resource (str "/feeds/" feed-id)]
     (api-call :get resource)))
+
+(defn create
+  "Create a new feed on Xively. Passed a map containing our values"
+  [feed]
+  (let [resource "/feeds"
+        feed (decorate-attributes feed)]
+    (api-call :post resource feed)))
